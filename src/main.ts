94c35954c0ad3f11b3846bc311278af0e838ace3
Rakesh D R <rakeshdr543@gmail.com>
2021-12-20 20:18:31 +0530

    Added deploy changes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new TransformInterceptor()), await app.listen(3000);
+  const port = process.env.PORT;
+  app.useGlobalInterceptors(new TransformInterceptor()), await app.listen(port);
 }
 bootstrap();
